a2b125f7ea2b46fd026af0df496e1eca6c41e31f
Johnson aka LQ's brother <58538723+damnphoreal@users.noreply.github.com>
2021-02-04 18:34:32 -0500

    next is to decypher rx

--- audioLED_signal.py
+++ audioLED_signal.py
@@ -13,25 +13,29 @@ right = LED(27) #gpio
 #for GPS
 import serial
 # option 1, using a lib
-# ser = serial.Serial(
-    # port='/dev/ttyS0',
-    # baudrate = 9600,
-    # parity=serial.PARITY_NONE,
-    # stopbits=serial.STOPBITS_ONE,
-    # bytesize=serial.EIGHTBITS,
-    # timeout=1
-# )
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0
+)
 # option 2, manual - try this first
-port = "/dev/serial0"
-ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
-data = ser.readline()
-
+#port = "/dev/ttyAMA0"
+#ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+while(True):
+    data = ser.readline()
+    print(data)
 #for buzzer
+"""
 import RPi.GPIO as GPIO
-buzzpin = 18
+buzzpin = 22
 GPIO.setmode(GPIO.BCM)
-while(true)
+GPIO.setup(buzzpin,GPIO.OUT
+for x in range(3):
     GPIO.output(buzzpin,GPIO.HIGH)
     sleep(0.5)
     GPIO.output(buzzpin,GPIO.LOW)
     sleep(0.5)
+"""
