5415468c23d15cc91e9ee519af1060a6755891e7
Johnson aka LQ's brother <58538723+damnphoreal@users.noreply.github.com>
2021-02-10 14:44:41 -0500

    left and right working

--- laptop testing lidar.py
+++ laptop testing lidar.py
@@ -31,7 +31,8 @@ def update_line(num, iterator, line):
     real_offsets = []
     real_intens = []
 
-    cartesian_arr = []  # x,y format
+    cartesian_arrL = []  # x,y format
+    cartesian_arrR = []  # x,y format
     degrees_arr = []
 
 
@@ -46,65 +47,107 @@ def update_line(num, iterator, line):
                 # degrees_arr.append((math.degrees(i[1]), i[2]))
                 real_intens.append(i[0])
 
-                cartesian_tuple = (i[2]*math.cos(i[1]), i[2]*math.sin(i[1]))  # (r*cos(degrees),r*sin(degrees))
-                cartesian_arr.append(cartesian_tuple)  # add to cartesian array
-                # print(cartesian_arr)
-                # print(degrees_arr)
+                if i[1] <= 1.5708: # this is the left
+                    cartesian_tupleL = (i[2]*math.cos(i[1]), i[2]*math.sin(i[1]))  # (r*cos(degrees),r*sin(degrees))
+                    cartesian_arrL.append(cartesian_tupleL)  # add to cartesian array
+                if i[1] >= 4.71239: # this is the left
+                    cartesian_tupleR = (i[2]*math.cos(i[1]), i[2]*math.sin(i[1]))  # (r*cos(degrees),r*sin(degrees))
+                    cartesian_arrR.append(cartesian_tupleR)  # add to cartesian array
+
 
 
     # do something with cartesian array now
 
     line_threshold = 200  # how long can you deviate from the original point
-    object_threshold = 15  # how many points to be an object, probably has to be higher
+    object_threshold = 15  # how many points to be an object, probably has to be higher // was 15
     object_wall = 40
-    object_count = 0
-    buffer = 0
+    object_countL = 0
+    object_countR = 0
 
-    start_point = cartesian_arr[0][0]  # x value, maybe fix cause what if the first point is bad
+    bufferL = 0
+    bufferR = 0
+
+    conditionL = 1
+    if len(cartesian_arrL) > 0:
+        start_pointL = cartesian_arrL[0][0]  # x value, maybe fix cause what if the first point is bad
+        compare_minusL = start_pointL - line_threshold
+        compare_plusL = start_pointL + line_threshold
+        xL = 1
+
+        while(conditionL and xL < len(cartesian_arrL)): # while the next x value is not +- 200 of the original
+            #  somehow this index gets out of range here, when i do something too close to the lidar?
+            if cartesian_arrL[xL][0] > compare_minusL and cartesian_arrL[xL][0] < compare_plusL:
+                object_countL = object_countL + 1
+            else:
+                bufferL = bufferL + 1
+            xL = xL + 1
+            if bufferL > 7:
+                conditionL = 0
+            #  getting a lot of conflicts, the bottom string keeps printing no matter what, works meh but there is a lot of flickering
+            #  zone feature might have to come in since i think it happens when it sees something else (i.e object 2)
 
 
-    x = 1
-    compare_minus = start_point - line_threshold
-    compare_plus = start_point + line_threshold
-    condition = 1
-    while(condition and x < len(cartesian_arr)): # while the next x value is not +- 200 of the original
-        #  somehow this index gets out of range here, when i do something too close to the lidar?
-        if cartesian_arr[x][0] > compare_minus and cartesian_arr[x][0] < compare_plus:
-            object_count = object_count + 1
+        last_pointL = cartesian_arrL[object_countL + bufferL-1][0] #this only works if there is a data point before the last buffer point
+
+
+        if object_countL > object_threshold and object_countL < object_wall:
+            print("LEFT !!! OBJECT DETECTED, LED ON !!!")
+        elif object_countL > object_wall:
+            print("LEFT UUU WALL PROBABLY, LED CAN TURN OFF NOW UUU")
         else:
-            buffer = buffer + 1
-        x = x + 1
-        if buffer > 7:
-            condition = 0
-        #  getting a lot of conflicts, the bottom string keeps printing no matter what, works meh but there is a lot of flickering
-        #  zone feature might have to come in since i think it happens when it sees something else (i.e object 2)
+            print("LEFT ??? NO OBJECT DETECTED, LED OFF ???")
+            #  this prints too often... it flickers
+
+    ###########################################copy starts#####################################################
+    conditionR = 1
+    if len(cartesian_arrR) > 0:
+        start_pointR = cartesian_arrR[0][0]  # x value, maybe fix cause what if the first point is bad
+        xR = 1
+        compare_minusR = start_pointR - line_threshold
+        compare_plusR = start_pointR + line_threshold
+
+        while(conditionR and xR < len(cartesian_arrR)): # while the next x value is not +- 200 of the original
+            #  somehow this index gets out of range here, when i do something too close to the lidar?
+            if cartesian_arrR[xR][0] > compare_minusR and cartesian_arrR[xR][0] < compare_plusR:
+                object_countR = object_countR + 1
+            else:
+                bufferR = bufferR + 1
+            xR = xR + 1
+            if bufferR > 7:
+                conditionR = 0
+            #  getting a lot of conflicts, the bottom string keeps printing no matter what, works meh but there is a lot of flickering
+            #  zone feature might have to come in since i think it happens when it sees something else (i.e object 2)
 
 
-    last_point = cartesian_arr[object_count + buffer-1][0]
+        last_pointR = cartesian_arrR[object_countR + bufferR-1][0]
 
 
-    if object_count > object_threshold and object_count < object_wall:
-        print("!!! OBJECT DETECTED, LED ON !!!")
-    elif object_count > object_wall:
-        print("UUU WALL PROBABLY, LED CAN TURN OFF NOW UUU")
-    else:
-        print("??? NO OBJECT DETECTED, LED OFF ???")
-        #  this prints too often... it flickers
+        if object_countR > object_threshold and object_countR < object_wall:
+            print("RIGHT !!! OBJECT DETECTED, LED ON !!!")
+        elif object_countL > object_wall:
+            print("RIGHT UUU WALL PROBABLY, LED CAN TURN OFF NOW UUU")
+        else:
+            print("RIGHT ??? NO OBJECT DETECTED, LED OFF ???")
+            #  this prints too often... it flickers
+
 
     #  30 = 0.523599
     #  330 = 5.75959
     #  print left or right
     #  greater than 30, less than 90
+    '''
     if(real_offsets[0][0] > 0.523599 and real_offsets[0][0] < 1.5708 and real_offsets[object_count + buffer -1][0] > 0.523599 and real_offsets[object_count+buffer-1][0] < 1.5708):
         print("LEFT SIDE LED")
     #  greater than 270, less than 330
     if(real_offsets[0][0] > 4.71239 and real_offsets[0][0] < 5.75959 and real_offsets[object_count + buffer -1][0] > 4.71239 and real_offsets[object_count+buffer-1][0] < 5.75959):
         print("RIGHT SIDE LED")
+    '''
 
 
-    with open(filename, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(cartesian_arr)
+
+    #with open(filename, 'w') as csvfile:
+    #    csvwriter = csv.writer(csvfile)
+    #    csvwriter.writerows(cartesian_arr)
 
     offsets = np.array(real_offsets)
     intens = np.array(real_intens)
